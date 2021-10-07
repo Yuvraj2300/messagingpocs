2eb51c1fdbcfec78c53d3aaa94beffa3d1d91e67
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2021-10-07 14:42:02 +0530

    Init on master

---
+++ jms-boot/src/main/java/com/sfg/jms/JmsBootApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsBootApplication.class, args);
+	}
+
+}
